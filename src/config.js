0596496193d3e47f25e5d81bca0afa5169873b78
Shweta Deshmukh <shweta.dskh@gmail.com>
2021-04-08 18:08:53 -0400

    adding all files

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+    
+    apiKey: "",
+    authDomain: "chat-medium-project.firebaseapp.com",
+    projectId: "chat-medium-project",
+    storageBucket: "chat-medium-project.appspot.com",
+    messagingSenderId: "167283905297",
+    appId: "1:167283905297:web:d756796d83b8ee262cb2b0"
+  };
